209cc4bfa8f43ca107ae9096b8b1ef9a3694619e
mor19-meet <mor19@meet.mit.edu>
2018-01-29 12:38:37 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Leaderboard(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80))
+    score = db.Column(db.Integer)
+
+
+db.create_all()
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
