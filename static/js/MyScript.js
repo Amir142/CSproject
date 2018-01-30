59c2269bd916d6b4b634c4938368b4f8689989ec
Amir142 <amiiir14.2@gmail.com>
2018-01-30 23:12:30 +0200

    exploring paperjs

--- static/js/MyScript.js
+++ static/js/MyScript.js
@@ -1,50 +1,34 @@
-var particles = [];
+var path;
+var pars = [];
+var color = new Color({hue: 360 * Math.random(), saturation: 1, brightness: 1});
 
-// function onMouseDrag(event){
-//     // The radius is the distance between the position
-//     // where the user clicked and the current position
-//     // of the mouse.
-//     var path = new Path.Circle({
-//         center: event.downPoint,
-//         radius: (event.downPoint - event.point).length,
-//         fillColor: 'white',
-//         strokeColor: 'black'
-//     });
-//
-//     // Remove this path on the next drag event:
-//     path.removeOnDrag();
-//     particles.push(path);
-//     console.log(particles);
+// function onMouseDown(event){
 // };
 
-var mouseDown = 0;
-document.body.onmousedown = function() {
-  ++mouseDown;
-  console.log(mouseDown);
-  var par = new Path.Circle({
-      center: event.downPoint,
-      radious: 0,
-      fillColor: 'red',
-      strokeColor: 'black'
-  })
-  while(mouseDown == 1){
-    par.radious = event.downPoint - event.point;
-    console.log(mouseDown);
+function onMouseDrag(event) {
+    // The radius is the distance between the position
+    // where the user clicked and the current position
+    // of the mouse.
+    par = new Path.Circle({
+        center: event.downPoint,
+        radius: (event.downPoint - event.point).length,
+        fillColor: color,
+        strokeColor: 'black'
+    });
+
+    // Remove this path on the next drag event:
+    par.removeOnDrag();
+};
+
+function onMouseUp(event){
+  color = new Color({hue: 360 * Math.random(), saturation: 1, brightness: 1});
+  pars.push(par);
+  console.log(pars);
+  move();
+};
+
+function move(){
+  for(p = 0; p < pars.length; p += 1){
+    pars[p].center = new Point(100,100);
   }
-}
-
-document.body.onmouseup = function(){
-  --mouseDown;
-}
-// function draw(){
-//   console.log("aa");
-  // var par = new Path.Circle({
-  //     center: event.downPoint,
-  //     radious: 0,
-  //     fillColor: new Color({ hue: Math.random() * 360, saturation: 1, brightness: 1 }),
-  //     strokeColor: 'black'
-  // })
-  // while(mouseDown){
-  //   par.radious = event.downPoint - event.point;
-  // }
-// };
+};
